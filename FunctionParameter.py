0a287a2031fac35d10c1fbf047565575575bc06c
Koray Zorluoglu <koray@d8devs.com>
2018-11-08 13:39:21 +0100

    Function with Parameters: Args and Params

--- FunctionParameter.py
+++ FunctionParameter.py
@@ -4,3 +4,19 @@ def multi_print(number = 3, word="Selam"):
 
 multi_print(5, 'Hello')
 multi_print(word="learning")
+
+
+# parameters as params
+def calculate_max(*params):
+    print(params)
+    print(params[0])
+
+
+calculate_max(2,3,4)
+
+# parameters as args
+
+def f(**args):
+    print(args)
+
+f(key="value", test=3)
